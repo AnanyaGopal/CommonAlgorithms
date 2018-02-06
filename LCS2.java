ad453a3e35e1cdc656e849ef00616419647a8224
AnanyaGopal <ananyagopal61@gmail.com>
2018-02-05 19:45:26 -0500

    Update LCS2.java
    Template fixed. - Running code uploaded.

--- LCS2.java
+++ LCS2.java
@@ -1,27 +1,41 @@
 import java.util.*;
 
-public class LCS2 {
-
-    private static int lcs2(int[] a, int[] b) {
-        //Write your code here
-        return Math.min(a.length, b.length);
+public class LCS2{
+ 
+	static int lcs2( int[] A, int[] B, int m, int n){
+		// If both are null - base case
+	    if (m == 0 || n == 0)
+	      return 0;
+	    if (A[m-1] == B[n-1])
+	    // if we find an equal ele - call recursively to next index	
+	      return 1 + lcs2(A, B, m-1, n-1);
+	    else
+	      return max(lcs2(A, B, m, n-1), lcs2(A, B, m-1, n));
+	 }
+	static int max(int a, int b){
+	    return (a > b)? a : b;
+	}
+  // driver method 	 
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    // First array of Integers
+    int size = scanner.nextInt();
+    
+    int[] a = new int[size];
+    for (int i = 0; i < size; i++) {
+        a[i] = scanner.nextInt();
     }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = scanner.nextInt();
-        }
-
-        int m = scanner.nextInt();
-        int[] b = new int[m];
-        for (int i = 0; i < m; i++) {
-            b[i] = scanner.nextInt();
-        }
-
-        System.out.println(lcs2(a, b));
+    // second array of ints
+    int sizeB = scanner.nextInt();
+    int[] b = new int[sizeB];
+    for (int i = 0; i < sizeB; i++) {
+        b[i] = scanner.nextInt();
     }
+    scanner.close();
+   
+    int m = a.length;
+    int n = b.length;
+ 
+    System.out.println(lcs2(a, b, m, n));
+  }
 }
-
